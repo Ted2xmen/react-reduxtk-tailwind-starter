b785be4ccbcaa41c4250d23b5cbde29e43a67ff6
Ted <tugrulerdemdogru@gmail.com>
2022-02-10 15:16:52 +0300

    react tailwind starter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-container h-screen flex justify-center items-center">
+      <div className="text-5xl font-bold bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 m-5 p-6">
+        React - Tailwind Starter
+      </div>
+      <h4 className="text-primary mt-19">
+        by <a href="https://github.com/Ted2xmen">Ted2xmen</a>
+      </h4>
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#FF9A02'
+      }
+    },
+  },
+  plugins: [],
+}
